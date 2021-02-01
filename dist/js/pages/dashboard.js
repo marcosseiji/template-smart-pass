f900666948bf4be83024b1d66288cabd6c03f550
Marcos Henrique Seiji <seiji.marcos@gmail.com>
2021-02-01 16:40:53 -0300

    Clean Code Examples

--- dist/js/pages/dashboard.js
+++ dist/js/pages/dashboard.js
@@ -127,25 +127,5 @@ $(function () {
     zIndex: 999999
   })
 
-  // bootstrap WYSIHTML5 - text editor
-  $('.textarea').summernote()
 
-  // $('.daterange').daterangepicker({
-  //   ranges: {
-  //     Today: [moment(), moment()],
-  //     Yesterday: [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-  //     'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-  //     'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-  //     'This Month': [moment().startOf('month'), moment().endOf('month')],
-  //     'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-  //   },
-  //   startDate: moment().subtract(29, 'days'),
-  //   endDate: moment()
-  // }, function (start, end) {
-  //   // eslint-disable-next-line no-alert
-  //   alert('You chose: ' + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-  // })
-
-  /* jQueryKnob */
-  $('.knob').knob()
 })
